b54cd347325e31043bcd8f70a8d9b1f588a4121d
chuoi2taps <thiennnph25438@fpt.edu.vn>
2024-08-06 20:59:26 +0700

    update Beginning

---
+++ src/js/beginning.js
@@ -0,0 +1,30 @@
+export function showBeginning() {
+  let result = "";
+  result += `<h1 class="text-center text-4xl font-bold uppercase outline-white">Lời nói đầu</h1>
+   <div class="mx-[50px]">
+     <ul class="list-decimal">
+       <li>
+         <h1 class="font-bold uppercase my-2">Lần đầu chơi diệt thần ?</h1>
+         <p>Khi mới bắt đầu chơi chế độ Diệt thần, chúng tôi khuyên bạn hãy lựa chọn 1-2 tướng tủ riêng cho bản thân.Bởi vì khi bạn vượt qua nhiệm vụ có độ khó Thường và Khó, chế độ Diệt thần sẽ không cho phép bạn đươc chọn trùng lặp tướng với đồng đội của mình. Chọn 2 lựa chọn phụ sẽ giúp bạn không rơi vào thế khó khăn khi chơi cùng đồng đội đã khóa tướng mình muốn. Kurumi là lựa chọn tốt nhất vì đó là tướng có độ thành thạo dễ nhất và có bộ skill buff và hồi phục cho đồng đội </p>
+       </li>
+       <li>
+         <h1 class="font-bold uppercase my-2">Làm sao để pro?</h1>
+         <ul class="list-disc">
+           <li>Việc học Mech boss và chơi nhiều để quen đòn sẽ khiến bạn mau pro hơn, khuyến khích nên đi solo tầng
+             1-25
+             để làm quen với các trùm trong tháp, không cần quá vội vàng dùng sen, hãy chơi lại nhiều lần để tay mau
+             to
+             lên. Và nếu cuối mùa mà farm không nổi thì cứ nhắn lên nhóm mọi người sẽ hỗ trợ bạn lên tầng 40 để có đủ
+             quà.
+           </li>
+           <li>
+             MỘT ĐIỀU QUAN TRỌNG NỮA LÀ HÃY NGHE MỌI NGƯỜI CALL NẾU BẠN LÀ NGƯỜI ĐƯỢC KÉO. ĐỪNG ĐÁNH THEO CẢM TỈNH VÌ
+             ĐIỀU ĐÓ SẼ LÀM CẢN TRỞ NGƯỜI KÉO RẤT NHIỀU DẪN ĐẾN TRẢI NGHIỆM GAME KHÔNG TỐT.
+           </li>
+         </ul>
+       </li>
+     </ul>
+   </div>`
+
+  return result;
+}
